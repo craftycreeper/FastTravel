2639d411cc1cd0db879c79ebea472cd5487afa56
craftycreeper <diagonalfish+craftycreeper@gmail.com>
2012-06-16 00:03:28 -0400

    Oops, a bit out of practice. Add /ftauto command file and re-add header to FastTravelSign.java.

---
+++ src/net/minebot/fasttravel/commands/FastTravelAutoCommand.java
@@ -0,0 +1,69 @@
+/*
+ * FastTravelSigns - The Simple Exploration and RPG-Friendly Teleportation Plugin
+ * 
+ * Copyright (c) 2011-2012 craftycreeper, minebot.net
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.minebot.fasttravel.commands;
+
+import net.minebot.fasttravel.FastTravelUtil;
+import net.minebot.fasttravel.data.FastTravelDB;
+import net.minebot.fasttravel.data.FastTravelSign;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FastTravelAutoCommand implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (sender instanceof Player && !((Player) sender).hasPermission("fasttravelsigns.auto")) {
+			return false;
+		}
+		
+		if (args.length == 0) {
+			FastTravelUtil.sendFTMessage(sender,
+					"You need to specify a fast travel sign.");
+		} else if (FastTravelDB.getSign(args[0]) == null) {
+			FastTravelUtil.sendFTMessage(sender, "No fast travel sign exists with that name.");
+		} else {
+			FastTravelSign sign = FastTravelDB.getSign(args[0]);
+			
+			if (!sign.isAutomatic()) {
+				sign.setAutomatic(true);
+				FastTravelUtil.sendFTMessage(sender, ChatColor.AQUA + sign.getName() + ChatColor.WHITE
+						+ " will now be available to all players (\"always on\").");
+			}
+			else {
+				sign.setAutomatic(false);
+				FastTravelUtil.sendFTMessage(sender, ChatColor.AQUA + sign.getName() + ChatColor.WHITE
+						+ " will no longer be available to all players.");
+			}
+		}
+		
+		return true;
+		
+	}
+
+}
